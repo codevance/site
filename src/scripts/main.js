64ba6f9e85a1087636a696beeb3fb8a9e3efeb4b
DESKTOP-D97LQ97\jluca <jlucaspaixao@outlook.com>
2018-04-26 16:40:45 -0300

    Desenvolvimento Desktop e Mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-require('bootstrap')
+import './jquery-global';
+import 'bootstrap';
+import 'owl.carousel';
+
+$(document).ready(function(){
+    $('.carousel-image').owlCarousel({
+        responsive:{
+            0:{
+                items: 1,
+                loop: true,
+                dots: false
+            }
+        }
+    });
+    $('.carousel-info').owlCarousel({
+        responsive:{
+            0:{
+                items: 1,
+                nav: true,
+                loop: true,
+                navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>']
+            }
+        }
+    });
+});
